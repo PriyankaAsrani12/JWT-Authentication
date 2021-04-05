e2edde2b52f237e970ff256b4870b68e7aef1050
Priyanka <priyankaasrani01234@gmail.com>
2021-04-06 00:10:11 +0530

    Initial commit

---
+++ authServer.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const jwt=require("jsonwebtoken");
+const dotenv=require("dotenv")
+
+const app=express();
+dotenv.config();
+
+app.use(express.json())
+
+const posts=[
+    {
+        username: "Priyanka",
+        post: "Post 1"
+    },
+    {
+        username: "Anjali",
+        post: "Post 2"
+    }
+]
+const refreshTokens=[]
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username
+    const user={name: username}
+    const accessToken=generateAccessToken(user)
+    const refreshtoken=jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshtoken)
+    res.json({accessToken:accessToken,refreshtoken:refreshtoken})
+})
+
+app.post("/token",(req,res)=>{
+    const refreshToken=req.body.token
+    if(refreshToken==null) return res.sendStatus(401)
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+        if(err){
+            return res.sendStatus(403)
+        }
+        const accessToken=generateAccessToken({name:user.name})
+        res.json({accessToken:accessToken})
+    })
+})
+
+function generateAccessToken(user){
+    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'30s'})
+}
+
+app.listen(3700,()=>{
+    console.log("Server is running on port 3700")
+})
---
+++ index.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const jwt=require("jsonwebtoken");
+const dotenv=require("dotenv")
+const app=express();
+dotenv.config();
+
+app.use(express.json())
+
+const posts=[
+    {
+        username: "Priyanka",
+        post: "Post 1"
+    },
+    {
+        username: "Anjali",
+        post: "Post 2"
+    }
+]
+
+app.get("/posts",authenticateToken,(req,res)=>{
+    res.json(posts.filter(post=>post.username===req.user.name))
+})
+
+app.post("/login",(req,res)=>{
+
+    const username=req.body.username
+    const user={name: username}
+
+    const accessToken=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken:accessToken})
+    
+})
+
+function authenticateToken(req,res,next){
+    const authHeader=req.headers['authorization']
+    const token=authHeader && authHeader.split(' ')[1]
+
+    if(token==null) return res.sendStatus(401)
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403)
+        req.user=user
+        next()
+
+    })
+
+}
+
+app.listen(3300,()=>{
+    console.log("Server is running on port 3300")
+})
